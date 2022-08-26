2801fec5ae721fcc6d5a849c80391594f9767153
isocodes51 <flyingcow51@hotmail.com>
2022-08-26 22:57:21 +0300

    Add project files.

---
+++ CSharpKamp2022/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpKamp2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Oop3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Oop3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //IhtiyacKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            //ihtiyacKrediManager.Hesapla();
+
+            //TasitKrediManager tasitKrediManager = new TasitKrediManager();
+            //tasitKrediManager.Hesapla();
+
+            //KonutKrediManager konutKrediManager = new KonutKrediManager();
+            //konutKrediManager.Hesapla();
+
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+            IKrediManager konutKrediManager = new KonutKrediManager();
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+           // tasitKrediManager.Hesapla();
+            BasvuruManager basvuru1 = new BasvuruManager();
+            //basvuru1.BasvuruYap(tasitKrediManager);
+            //basvuru1.BasvuruYap(konutKrediManager);
+            List<IKrediManager> krediler = new List<IKrediManager>() {ihtiyacKrediManager, tasitKrediManager, konutKrediManager };
+            basvuru1.KrediOnBilgilendirmesiYap(krediler);
+
+        }
+    }
+}
