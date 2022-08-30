c8e5097037b779d87ea1492c415a9452b0d6e5d7
IsoCodes <84030162+isocodes51@users.noreply.github.com>
2022-08-30 22:25:19 +0300

    Update Odev6_SQL.sql

--- Odev6_SQL.sql
+++ Odev6_SQL.sql
@@ -1,7 +1,7 @@
 select p.ProductName as 'Ürün Adı', 
 sum(od.UnitPrice*od.Quantity) as 'Toplam Kazanç Miktarı'
-from ([Order Details] as od
+from (([Order Details] as od
 inner join Products as p on p.ProductID=od.ProductID)
-inner join Orders as o on o.OrderID=od.OrderID
+inner join Orders as o on o.OrderID=od.OrderID)
 group by p.ProductName
 order by sum(od.UnitPrice*od.Quantity)  desc
